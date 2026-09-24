96e7dd1a8a991102fcd619924c8a5b9bed5134fa
vladimirpavlov812 <vladimir.pavlov812@gmail.com>
2026-09-24 14:57:02 +0200

    Remove duplicate Yandex Metrika initialization

--- src/App.jsx
+++ src/App.jsx
@@ -188,28 +188,6 @@ export default function App() {
   };
 }, []);
 
-    // === Яндекс.Метрика ===
-  useEffect(() => {
-    const METRIKA_ID = 105378247;
-
-    // Подключение скрипта
-    (function(m,e,t,r,i,k,a){
-      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      m[i].l=1*new Date();
-      k=e.createElement(t),a=e.getElementsByTagName(t)[0];
-      k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
-    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    // Инициализация счётчика
-    window.ym(METRIKA_ID, "init", {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true
-    });
-
-  }, []);
-
   const handleOpenPrioritization = () => {
   if (!treeData) return;
   setShowPriorModal(true);
